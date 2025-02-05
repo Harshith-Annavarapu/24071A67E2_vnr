dd3237f20d4b1c9145259380937ff938b9e0047f
Harshith-Annavarapu <24071a67e2@vnrvjiet.in>
2025-02-06 01:24:33 +0530

    Created using Colab

--- S1/5.py
+++
@@ -1,8 +0,0 @@
-def remove_duplicates(input_list):
-    return list(set(input_list))
-
-
-if __name__ == "__main__":
-    sample_list = [1, 2, 2, 3, 4, 4, 5]
-    print("Original list:", sample_list)
-    print("List after removing duplicates:", remove_duplicates(sample_list))
--- S1/6.py
+++
@@ -1,8 +0,0 @@
-my_dict = {
-    "name": "Alice",
-    "age": 30,
-    "city": "New York",
-    "occupation": "Engineer",
-    "hobby": "Painting"
-}
-print(my_dict)
